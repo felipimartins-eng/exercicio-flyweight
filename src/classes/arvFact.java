04ed66c3eedd9643b1e4d8fe7a2e0264d32d40c3
felipiUnifesp <felipi.martins@unifesp.br>
2026-05-22 21:43:28 -0300

    implementei factory com um hashmap/cache de especies

--- src/classes/arvFact.java
+++ src/classes/arvFact.java
@@ -1,4 +1,12 @@
 package classes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class arvFact {
+    static Map<String, arvData> mapaEspecies = new HashMap<>();
+
+    public static arvData getTreeType(String especie, String cor, String miscData) {
+        return mapaEspecies.computeIfAbsent(especie, e -> new arvData(e, cor, miscData));
+    }
 }
