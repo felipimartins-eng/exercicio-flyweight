12f90f738f19d2bfe938d76037d56e89ae6397de
felipiUnifesp <felipi.martins@unifesp.br>
2026-05-22 22:20:46 -0300

    contator de bytes

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import classes.arvData;
 import classes.floresta;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.lang.instrument.Instrumentation;
 
 public class Main {
     public static void main(String[] args) {
         int numArvores = 1000000;
         int posRange = 500;
+        int tamEstrutura = 96; // Assumindo que cada especie possua um limite de 85 caracteres em cada um dos três campos de 'arvData'
 
         floresta floresta = new floresta();
         List<String> especiesArvore = new ArrayList<>(){};
@@ -15,7 +18,8 @@ public class Main {
         especiesArvore.add("Mogno");
         especiesArvore.add("Arvore-Magica-Da-Sabedoria");
 
-        double distribuicaoEspecies = (double) numArvores / (double) especiesArvore.size();
+        int numEspecies = especiesArvore.size();
+        double distribuicaoEspecies = (double) numArvores / (double) numEspecies;
 
         for (String especieNome : especiesArvore) {
             for (int j = 0; j < Math.floor(distribuicaoEspecies); j++) {
@@ -25,5 +29,7 @@ public class Main {
             }
         }
 
+        System.out.println("Tamanho atual utilizado pelos dados compartilhados: " + numEspecies * tamEstrutura + " Bytes");
+        System.out.println("Tamanho hipotetico utilizado pelos dados compartilhados: " + numArvores * tamEstrutura + " Bytes");
     }
 }
