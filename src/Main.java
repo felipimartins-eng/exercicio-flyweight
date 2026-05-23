eafd2ed82b3633e0590498a537568d983685e97c
felipiUnifesp <felipi.martins@unifesp.br>
2026-05-22 22:07:52 -0300

    implementei main e criacao de arvores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import classes.floresta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
+        int numArvores = 1000000;
+        int posRange = 500;
+
+        floresta floresta = new floresta();
+        List<String> especiesArvore = new ArrayList<>(){};
+        especiesArvore.add("Eucalipto");
+        especiesArvore.add("Mogno");
+        especiesArvore.add("Arvore-Magica-Da-Sabedoria");
+
+        double distribuicaoEspecies = (double) numArvores / (double) especiesArvore.size();
+
+        for (String especieNome : especiesArvore) {
+            for (int j = 0; j < Math.floor(distribuicaoEspecies); j++) {
+                int x = ThreadLocalRandom.current().nextInt(-posRange, posRange + 1);
+                int y = ThreadLocalRandom.current().nextInt(-posRange, posRange + 1);
+                floresta.arvCriar(x, y, especieNome, especieNome, "arbitrario");
+            }
+        }
 
     }
 }
